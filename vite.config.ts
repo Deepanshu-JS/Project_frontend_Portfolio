2a06663ab8b52d91544907f617c4134bc5a90ead
Deepanshu Singh <singhdepanshu786@gmail.com>
2026-03-05 00:10:16 +0530

    config updated to production

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Project_frontend_Portfolio/',
   server: {
     host: "::",
     port: 8080,
