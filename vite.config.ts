e10c322cdbeaf368a55bad69ccd89e135e9883f1
Deepanshu Singh <singhdepanshu786@gmail.com>
2026-03-05 00:19:15 +0530

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Project_frontend_Portfolio/',
+  base: '/project_frontend_portfolio/',
   server: {
     host: "::",
     port: 8080,
