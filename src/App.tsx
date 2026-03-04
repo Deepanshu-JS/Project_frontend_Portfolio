db2b298be23a1b6f9bef7b12275c677e5184ee3b
Deepanshu Singh <singhdepanshu786@gmail.com>
2026-03-05 00:23:14 +0530

    Fix routing with BrowserRouter basename

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
